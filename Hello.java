230b172f3f895b5aabfbe2172376611e5d14d01c
kingykwang <kingykwang@tencent.com>
2023-02-28 15:09:03 +0800

    提交hello

---
+++ Hello.java
@@ -0,0 +1,9 @@
+package myjava;
+
+public class Hello {
+    public static void main(String[] args) {
+        test.test1("null");
+        test.test1("args");
+        System.out.println("hello world");
+    }
+}
