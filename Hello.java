7a5fdda223cb67741e4883abac63df09d563d8a7
kingykwang <kingykwang@tencent.com>
2023-02-28 15:56:38 +0800

    删除无用文件

--- Hello.java
+++ Hello.java
@@ -2,8 +2,6 @@ package myjava;
 
 public class Hello {
     public static void main(String[] args) {
-        test.test1("null");
-        test.test1("args");
         System.out.println("hello world");
     }
 }
--- test.java
+++
@@ -1,7 +0,0 @@
-package myjava;
-
-public class test {
-    public static void test1(String args) {
-        System.out.println(args);
-    }
-}
