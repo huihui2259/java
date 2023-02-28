ba8afb806d0e312aace10c4ac5390004c536d6f1
kingykwang <kingykwang@tencent.com>
2023-02-28 15:50:05 +0800

    旋转图像

---
+++ Rotate.java
@@ -0,0 +1,24 @@
+package myjava;
+
+public class Rotate {
+    public void rotate(int[][] matrix) {
+        int n = matrix.length;
+        int l = 0, r = n - 1;
+        while (l < r) {
+            for (int i = 0; i < n; i++) {
+                int tmp = matrix[l][i];
+                matrix[l][i] = matrix[r][i];
+                matrix[r][i] = tmp;
+            }
+            l++;
+            r--;
+        }
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                int tmp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = tmp;
+            }
+        }
+    }
+}
