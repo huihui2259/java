9a16366a81902ade1ccb53c892fd8f4c1ca7c31b
kingykwang <kingykwang@tencent.com>
2023-03-06 16:39:17 +0800

    工具类

--- Util.java
+++ Util.java
@@ -1,6 +1,8 @@
 package myjava;
 
 public class Util {
+
+    // 打印二维数组
     public static void PrintTwo(int[][] res) {
         for (int i = 0; i < res.length; i++) {
             for (int j = 0; j < res[0].length; j++) {
