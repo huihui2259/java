d72030d738d49e623bde56e733cd146f3734360e
kingykwang <kingykwang@tencent.com>
2023-03-14 16:34:43 +0800

    二维数组中的查找

--- offer/FindNumberIn2DArray.java
+++ offer/FindNumberIn2DArray.java
@@ -2,6 +2,22 @@ package myjava.offer;
 
 public class FindNumberIn2DArray {
     public boolean findNumberIn2DArray(int[][] matrix, int target) {
-        return true;
+        int m = matrix.length;
+        if (m == 0)
+            return false;
+        int n = matrix[0].length;
+        if (n == 0)
+            return false;
+        int i = m - 1, j = 0;
+        while (i >= 0 && j < n) {
+            if (target > matrix[i][j]) {
+                j++;
+            } else if (target < matrix[i][j]) {
+                i--;
+            } else {
+                return true;
+            }
+        }
+        return false;
     }
 }
